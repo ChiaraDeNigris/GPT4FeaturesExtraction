9a641ee00efadf7eb0cab1b20ca5e68fc79a72ee
Chiara De Nigris <79252190+ChiaraDeNigris@users.noreply.github.com>
2025-02-24 10:00:05 +0100

    Update Preprocessing.py

--- code/Preprocessing.py
+++ code/Preprocessing.py
@@ -195,7 +195,6 @@ from pyspark.sql.functions import col, when, regexp_replace
 
 # Define the sensitive data patterns
 CODICE_FISCALE = r'\b(?:[A-Za-z][AEIOUaeiou][AEIOUXaeioux]|[AEIOUaeiou]X{2}|[B-DF-HJ-NP-TV-Zb-df-hj-np-tv-z]{2}[A-Za-z]){2}(?:[\dLMNP-Vlmnp-v]{2}(?:[A-EHLMPR-Ta-ehlmpr-t](?:[04LQ][1-9MNP-Vlmnp-v]|[15MR][\dLMNP-Vlmnp-v]|[26NS][0-8LMNP-Ulmnp-u])|[DHPSdhps][37PTpt][0L]|[ACELMRTacelmr-t][37PTpt][01LM]|[AC-EHLMPR-Tac-ehlmpr-t][26NSns][9Vv])|(?:[02468LNQSUlnqsu][048LQU048lqu]|[13579MPRTV13579mprtv][26NS26ns])B[26NS26ns][9Vv])(?:[A-MZa-mz][1-9MNP-Vlmnp-v][\dLMNP-Vlmnp-v]{2}|[A-Ma-m][0L](?:[1-9MNP-Vlmnp-v][\dLMNP-Vlmnp-v]|[0L][1-9MNP-Vlmnp-v]))[A-Za-z]\b'
-EMAIL = r'\b(?!.*lottomatica)[\w\-\.]+@([\w-]+\.)+[\w-]{2,}\b'
 IBAN = r'\b[A-Za-z]{2}[0-9]{2}(?:[ ]?[0-9]{4}){4}(?!(?:[ ]?[0-9]){3})(?:[ ]?[0-9]{1,2})?\b'
 VISA = r'\b4[0-9]{12}(?:[0-9]{3})?\b'
 MASTERCARD = r'\b(5[1-5][0-9]{14}|2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12}))\b'
@@ -285,7 +284,6 @@ df = df.apply(lambda x: x.astype(str))
 
 #Pattern per trovare i dati sensibili
 CODICE_FISCALE = r'\b(?:[A-Za-z][AEIOUaeiou][AEIOUXaeioux]|[AEIOUaeiou]X{2}|[B-DF-HJ-NP-TV-Zb-df-hj-np-tv-z]{2}[A-Za-z]){2}(?:[\dLMNP-Vlmnp-v]{2}(?:[A-EHLMPR-Ta-ehlmpr-t](?:[04LQ][1-9MNP-Vlmnp-v]|[15MR][\dLMNP-Vlmnp-v]|[26NS][0-8LMNP-Ulmnp-u])|[DHPSdhps][37PTpt][0L]|[ACELMRTacelmr-t][37PTpt][01LM]|[AC-EHLMPR-Tac-ehlmpr-t][26NSns][9Vv])|(?:[02468LNQSUlnqsu][048LQU048lqu]|[13579MPRTV13579mprtv][26NS26ns])B[26NS26ns][9Vv])(?:[A-MZa-mz][1-9MNP-Vlmnp-v][\dLMNP-Vlmnp-v]{2}|[A-Ma-m][0L](?:[1-9MNP-Vlmnp-v][\dLMNP-Vlmnp-v]|[0L][1-9MNP-Vlmnp-v]))[A-Za-z]\b'
-EMAIL = r'\b(?!.*lottomatica)[\w\-\.]+@([\w-]+\.)+[\w-]{2,}\b'
 IBAN= r'\b[A-Za-z]{2}[0-9]{2}(?:[ ]?[0-9]{4}){4}(?!(?:[ ]?[0-9]){3})(?:[ ]?[0-9]{1,2})?\b'
 VISA= r'\b4[0-9]{12}(?:[0-9]{3})?\b'
 MASTERCARD= r'\b(5[1-5][0-9]{14}|2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12}))\b'
